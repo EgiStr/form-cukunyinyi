6828707947709151fe900183ee79bcd75c16f4fc
diosamuel <koding12sam@gmail.com>
2024-11-05 12:02:40 +0700

    update navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import logo from '@/assets/logo.png'
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
@@ -25,15 +25,15 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-white shadow-md py-4 px-8 flex justify-between items-center fixed top-0 left-0 right-0 z-50">
+      <nav className="bg-white shadow-md py-1 md:px-8 flex justify-between items-center fixed top-0 left-0 right-0 z-50">
         <div className="flex items-center space-x-2">
-          <img src="/path-to-logo.png" alt="" className="h-6" />
-          <span className="text-gray-700 font-semibold text-lg">Ekowisata Cuka Nyinyi</span>
+          <img src={logo} alt="" className="h-14" />
+          <span className="text-gray-700 font-semibold">Ekowisata Cuku Nyi Nyi</span>
         </div>
 
-        <div className="flex space-x-6 text-gray-600">
-          <a href="#knowMangrove" className="hover:text-gray-800">knowMangrove</a>
-          <a href="#daftarWisata" className="hover:text-gray-800">Daftar Wisata</a>
+        <div className="flex items-center space-x-6 text-gray-600">
+          <a href="/scan" className="hover:text-gray-800">Kenali Mangrove</a>
+          <a href="/" className="hover:text-gray-800">Daftar Wisata</a>
           {isLoggedIn ? (
             <button
               onClick={handleLogout}
